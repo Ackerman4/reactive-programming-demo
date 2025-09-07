006240f54157485f898d47f575652377639dff6f
AJ <moralesaj03@gmail.com>
2025-09-07 20:07:44 +0800

    Added Demo for Mono and Flux Operations with Unit Tests

--- src/main/java/com/example/reactive_programming_demo/ReactiveProgrammingDemoApplication.java
+++ src/main/java/com/example/reactive_programming_demo/ReactiveProgrammingDemoApplication.java
@@ -1,13 +1,20 @@
 package com.example.reactive_programming_demo;
 
+import com.example.reactive_programming_demo.service.FluxAndMonoGeneratorServiceDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 public class ReactiveProgrammingDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReactiveProgrammingDemoApplication.class, args);
-	}
 
+        FluxAndMonoGeneratorServiceDemo fluxAndMonoGeneratorService = new FluxAndMonoGeneratorServiceDemo();
+        fluxAndMonoGeneratorService.namesFlux();
+
+//        fluxAndMonoGeneratorService.nameMono()
+//                .subscribe(System.out::println);
+	}
 }
